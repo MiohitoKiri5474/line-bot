03d1015eed229e18c08e35ef7787f31f289a2b60
miohitokiri5474 <lltzpp>
2022-12-25 13:58:44 +0800

    fix random bug

--- mtg_cards_fetcher/views.py
+++ mtg_cards_fetcher/views.py
@@ -108,7 +108,7 @@ def callback ( request ):
                     print ( cards.name() )
                     storge_card_name ( event.source.user_id, cards.name() )
 
-                    rpy = full_cards_information ( cards )
+                    rpy = TextSendMessage ( full_cards_information ( cards ) )
                 elif event.message.text.lower() == 'oracle':
                     name = get_card_name ( event.source.user_id )
                     if name == '':
